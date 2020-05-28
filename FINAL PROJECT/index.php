e6c01efc3d33f3b5ff29e742a54d1ccdd5920125
gevinjanitto <51157180+gevinjanitto@users.noreply.github.com>
2020-05-28 18:52:19 +0800

    Create index.php

---
+++ FINAL PROJECT/index.php
@@ -0,0 +1,336 @@
+<!DOCTYPE html>
+<html lang="zxx" class="no-js">
+<head>
+	<!-- Mobile Specific Meta -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<!-- Favicon-->
+	<link rel="shortcut icon" href="img/fav.png">
+	<!-- Author Meta -->
+	<meta name="author" content="CodePixar">
+	<!-- Meta Description -->
+	<meta name="description" content="">
+	<!-- Meta Keyword -->
+	<meta name="keywords" content="">
+	<!-- meta character set -->
+	<meta charset="UTF-8">
+	<!-- Site Title -->
+	<title>WEB SISTEM INFORMASI UNUD</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Poppins:300,500,600" rel="stylesheet">
+		<!--
+		CSS
+		============================================= -->
+		<link rel="stylesheet" href="css/linearicons.css">
+		<link rel="stylesheet" href="css/owl.carousel.css">
+		<link rel="stylesheet" href="css/font-awesome.min.css">
+		<link rel="stylesheet" href="css/nice-select.css">
+		<link rel="stylesheet" href="css/magnific-popup.css">
+		<link rel="stylesheet" href="css/bootstrap.css">
+		<link rel="stylesheet" href="css/main.css">
+	</head>
+	<body>
+		<div class="main-wrapper-first">
+			<header>
+				<div class="container">
+					<div class="header-wrap">
+						<div class="header-top d-flex justify-content-between align-items-center">
+							<div class="logo">
+								<a href="index.html"><img src="img/logo.png" alt=""></a>
+							</div>
+							<div class="main-menubar d-flex align-items-center">
+								<nav class="hide">
+									<a href="#">Home</a>
+									<a href="#visi">Visi</a>
+									<a href="#galery">Galery</a>
+									<a href="#sejarah">Sejarah</a>
+									<a href="#login">Login</a>
+									<a href="#about">About</a>
+								</nav>
+								<div class="menu-bar"><span class="lnr lnr-menu"></span></div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</header>
+			<div class="banner-area">
+				<div class="container">
+					<div class="row justify-content-center height align-items-center">
+						<div class="col-lg-8">
+							<div class="banner-content text-center">
+								<span class="text-white top text-uppercase">SISTEM INFORMASI</span>
+								<h1 class="text-white text-uppercase">UNIVERSITAS UDAYANA</h1>
+								<a href="#login" class="primary-btn d-inline-flex align-items-center"><span class="mr-10">LOGIN</span><span class="lnr lnr-arrow-right"></span></a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<!-- Start home -->
+			<section id="visi" class="featured-area">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-4">
+							<div class="single-feature d-flex flex-wrap justify-content-between">
+								<div class="icon d-flex align-items-center justify-content-center">
+									<span class="lnr lnr-eye"></span>
+								</div>
+								<div class="desc">
+									<h6 class="title text-uppercase">Visi</h6>
+									<p>Terwujudnya perguruan tinggi yang unggul, mandiri, dan berbudaya.</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="single-feature d-flex flex-wrap justify-content-between">
+								<div class="icon d-flex align-items-center justify-content-center">
+									<span class="lnr lnr-rocket"></span>
+								</div>
+								<div class="desc">
+									<h6 class="title text-uppercase">Misi</h6>
+									<p>Menghasilkan karya inovatif dan prospektif bagi kemajuan Unud serta perekonomian nasional </p>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="single-feature d-flex flex-wrap justify-content-between">
+								<div class="icon d-flex align-items-center justify-content-center">
+									<span class="lnr lnr-diamond"></span>
+								</div>
+								<div class="desc">
+									<h6 class="title text-uppercase">Tujuan</h6>
+									<p>Menghasilkan lulusan bermutu yang memiliki kompetensi tinggi dalam penguasaan ilmu pengetahuan, teknologi, dan seni</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+			<!-- End home -->
+			<!-- Start galeri -->
+		</div>
+		<div class="main-wrapper">
+			<section id="galery" class="amazing-works-area">
+					<div class="row justify-content-center">
+						<div class="col-lg-8">
+							<div class="section-title text-center">
+								<h3>GALERY</h3>
+								<span class="text-uppercase">EVENT DI UNIVERSITAS UDAYANA</span>
+							</div>
+						</div>
+					</div>
+					<div class="active-works-carousel mt-40">
+						<div class="item">
+							<div class="thumb" style="background: url(img/w1.jpg);"></div>
+							<div class="caption text-center">
+								<h6 class="text-uppercase">Student Day 2017</h6>
+								<p>Merupakan acara pengenalan kampus yang diselenggarakan oleh BEM Udayana untuk mahasiswa baru</p>
+							</div>
+						</div>
+						<div class="item">
+							<div class="thumb" style="background: url(img/w2.jpg);"></div>
+							<div class="caption text-center">
+								<h6 class="text-uppercase">Dies Natalis 2019</h6>
+								<p>Merupakan acara taunan dari BEM Udayana untuk memperingati hari Ulang Tahun Universitas Udayana dengan mengadakan perlombaan antar fakultas</p>
+							</div>
+						</div>
+						<div class="item">
+							<div class="thumb" style="background: url(img/w3.jpg);"></div>
+							<div class="caption text-center">
+								<h6 class="text-uppercase">Udayana Jazz Festival ke 9</h6>
+								<p>Merupakan acara taunan BEM udayana untuk mengadakan konser musik yang mengundang artis ternama, pada taun itu ada Raisa dan Isyana</p>
+							</div>
+						</div>
+						<div class="item">
+							<div class="thumb" style="background: url(img/w4.jpg);"></div>
+							<div class="caption text-center">
+								<h6 class="text-uppercase">PMU ke 7</h6>
+								<p>Merupakan acara taunan BEM udayana untuk melatih kepemimpinan mahasiswa baru yang nantinya diharapkan untuk meneruskan BEM</p>
+							</div>
+						</div>
+						<div class="item">
+							<div class="thumb" style="background: url(img/w5.jpg);"></div>
+							<div class="caption text-center">
+								<h6 class="text-uppercase">INVENTION 2018</h6>
+								<p>Merupakan perlombaan IT antar SMA dan Universitas tingkat Nasional yang diselenggarakan oleh Prodi Informatika</p>
+							</div>
+						</div>
+						<div class="item">
+							<div class="thumb" style="background: url(img/w6.jpg);"></div>
+							<div class="caption text-center">
+								<h6 class="text-uppercase">Udayana Jazz Vestival ke 8</h6>
+								<p>Merupakan acara taunan BEM udayana untuk mengadakan konser musik yang mengundang artis ternama, pada taun itu ada HIVI dan Glen Fredly</p>
+							</div>
+						</div>
+					</div>
+			</section>
+		</div>
+		<div class="main-wrapper">
+			<!-- End galery -->
+			<!-- Start Sejarah -->
+			<section id="sejarah" class="story-area">
+				<div class="container">
+					<div class="row align-items-center">
+						<div class="col-lg-3">
+							<div class="story-title">
+								<h3 class="text-white">Sejarah</h3>
+								<span class="text-uppercase text-white">Universitas Udayana</span>
+							</div>
+						</div>
+						<div class="col-lg-6">
+							<div class="story-box">
+								<h6 class="text-uppercase">Sejarah universitas Udayana</h6>
+								<p>Cikal bakal Unud adalah Fakultas Sastra Udayana cabang Universitas Airlangga yang diresmikan oleh P. J. M. Presiden Republik Indonesia Ir. Soekarno, dibuka oleh J. M. Menteri P.P dan K. Prof. DR. Priyono pada tanggal 29 September 1958 sebagaimana tertulis pada Prasasti di Fakultas Sastra Jalan Nias Denpasar. </p>
+								<a href="#" class="primary-btn hover d-inline-flex align-items-center"><span class="lnr lnr-history">&nbsp&nbsp&nbsp&nbsp</span><span class="mr-10">29 September 1958</span></span></a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+			<!-- End sejarah -->
+			<!-- Start login -->
+			<section id="login" class="subscription-area">
+				<div class="container">
+					<div class="row justify-content-center">
+						<div class="col-lg-8">
+							<div class="section-title text-center">
+								<h3>Login</h3>
+								<span class="text-uppercase">Silahkan masukan email dan password anda</span>
+							</div>
+						</div>
+					</div>
+					<div class="row justify-content-center">
+						<div class="col-lg-6">
+							<div id="mc_embed_signup">
+								<form target="_blank" novalidate action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&id=92a4423d01" method="get" class="subscription relative">
+									<input type="email" name="email" placeholder="Email address" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Email address'" required>
+									<input type="email" name="pass" placeholder="Password" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Password'" required>
+									<button class="primary-btn hover d-inline-flex align-items-center"><span class="mr-10">Login</span><span class="lnr lnr-arrow-right"></span></button>
+									<div class="info"></div>
+								</form>
+								<br><a class="nav-link" data-toggle="modal" data-target="#daftar" style="cursor: pointer;">Daftar Mahasiswa?</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+			<!-- End login -->
+			<!-- Start tentang -->
+			<section id="about" class="footer-widget-area">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-4">
+							<div class="single-widget d-flex flex-wrap justify-content-between">
+								<div class="icon d-flex align-items-center justify-content-center">
+									<span class="lnr lnr-pushpin"></span>
+								</div>
+								<div class="desc">
+									<h6 class="title text-uppercase">Address</h6>
+									<p>Jl. Raya Kampus UNUD, Bukit Jimbaran, Kuta Selatan, Badung-Bali-80361</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="single-widget d-flex flex-wrap justify-content-between">
+								<div class="icon d-flex align-items-center justify-content-center">
+									<span class="lnr lnr-earth"></span>
+								</div>
+								<div class="desc">
+									<h6 class="title text-uppercase">Email Address</h6>
+									<div class="contact">
+										<a href="mailto:info@unud.ac.id">info@unud.ac.id</a> <br>
+										<a href="mailto:gevinjanitto@gmail.com">gevinjanitto@gmail.com</a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="single-widget d-flex flex-wrap justify-content-between">
+								<div class="icon d-flex align-items-center justify-content-center">
+									<span class="lnr lnr-phone"></span>
+								</div>
+								<div class="desc">
+									<h6 class="title text-uppercase">Phone Number</h6>
+									<div class="contact">
+										<a href="tel:+62 (361) 701812">+62 (361) 701812</a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+			<!-- End tentang -->
+			<!-- Start footer -->
+			<footer>
+				<div class="container">
+					<div class="footer-content d-flex justify-content-between align-items-center flex-wrap">
+						<div class="logo">
+							<img style="width:50px;"src="img/logo.png" alt="">
+						</div>
+						<div class="copy-right-text">Copyright &copy; 2017  |  All rights reserved to Udayana University </div>
+						<div class="footer-social">
+							<a href="https://web.facebook.com/?_rdc=1&_rdr" target="_blank"><i class="fa fa-facebook"></i></a>
+							<a href="https://twitter.com/" target="_blank"><i class="fa fa-twitter"></i></a>
+							<a href="https://www.unud.ac.id/ target="_blank"><i class="fa fa-dribbble"></i></a>
+						</div>
+					</div>
+				</div>
+			</footer>
+			<!-- End footer-->
+		</div>
+		<script src="js/vendor/jquery-2.2.4.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+		<script src="js/vendor/bootstrap.min.js"></script>
+		<script src="js/jquery.ajaxchimp.min.js"></script>
+		<script src="js/owl.carousel.min.js"></script>
+		<script src="js/jquery.nice-select.min.js"></script>
+		<script src="js/jquery.magnific-popup.min.js"></script>
+		<script src="js/main.js"></script>
+	</body>
+</html>
+ <form name="daftar" onsubmit="return checkform()" method="post" action="proses/masukdatanasabah.php">
+    <div class="modal fade" id="daftar" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Form Pendaftaran</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+   	    	  <div class="mt-10">
+		        <label>Email</label>
+				<input type="text" type="email" id="email" name="email" class="single-input-primary" placeholder="Masukan e-mail" required>
+		      </div>
+		      <div class="mt-10">
+				<label >Nama Lengkap</label>
+				<input type="text" name="nama" class="single-input-primary" placeholder="Masukan Nama Lengkap" required>
+		      </div>
+		      <div class="mt-10">
+				 <label >NIM</label>
+				 <input type="text" name="NIM" class="single-input-primary" placeholder="Masukan NIM Anda" required>
+			  </div>
+			 <div class="mt-10">
+				 <label >Password</label>
+				 <input type="password" name="pass" id="cekpas1" class="single-input-primary" placeholder="Masukan Password" required>
+			  </div>
+			  <div class="mt-10">
+				 <label >Confirm Password</label>
+				 <input type="password" name="pass1" id="cekpas2" class="single-input-primary" placeholder="Masukan Kembali Password" required>
+			  </div>
+			  <div class="mt-10">
+			  <label>
+				 <input type="checkbox" required> I accept the Terms of Use & Privacy Policy.
+				 <span class="checkmark"></span>
+			  </label>
+	          </div><br><br>
+          <div class="modal-footer">
+            <button type="reset" class="genric-btn info radius">Reset</button>
+            <button type="submit" class="genric-btn success radius"><span class="mr-10">Daftar</span></button>
+          </div>
+        </div>
+      </div>
+    </div>
+    </form>
